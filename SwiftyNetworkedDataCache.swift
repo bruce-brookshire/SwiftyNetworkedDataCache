f3e65f60563fa7577757f37f67abb7a7f0c9bcfa
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2018-05-04 20:31:34 -0500

    Switched over from requiring a whitebox framework approach of overriding a class to a more blackbox framework approach of implementing a protocol. Makes it easier on the user and harder on everyone else lol

--- SwiftyNetworkedDataCache.swift
+++ SwiftyNetworkedDataCache.swift
@@ -1,3 +1,167 @@
+///Allows the DataCache to find and cache the data at the url specified
+public protocol CachedDataOwner: Hashable {
+    ///url path to download data from
+    var urlPath: String {get}
+    
+    ///Datatype for the the cache
+    associatedtype ProcessedData
+    
+    ///Used to process the data received from a network request
+    static func processData(data: Data) -> ProcessedData?
+}
+
+///The result type of the data fetch operation
+enum CacheFetchResult {
+    case CACHED, FETCHED, BADREQUEST, DATAPROCESSINGISSUE
+}
+
+///Used to limit the size of the data cache
+///while minimizing the number of network requests
+open class DataCache<T:CachedDataOwner> {
+    
+    typealias ProcessedData = T.ProcessedData
+    private var dataCache: CappedCache<T>
+    fileprivate var parentWrapperDict: [Int: DataWrapper<T>] = [:]
+    
+    init(maxSize: Int) {
+        self.dataCache = CappedCache<T>(maxSize: maxSize)
+    }
+    
+    ///Returns the profile photo if available.
+    /// - parameter parent: parent object to fetch data for
+    /// - parameter completion: NOT GUARANTEED TO RUN ON MAIN THREAD
+    func fetchData(forParent parent: T, _ completion: @escaping (ProcessedData?, CacheFetchResult) -> Void) {
+        let wrapper: DataWrapper<T>
+        
+        if let dataWrapper = parentWrapperDict[parent.hashValue] {
+            wrapper = dataWrapper
+        } else {
+            let dataWrapper = DataWrapper<T>(parent, self)
+            parentWrapperDict[parent.hashValue] = dataWrapper
+            wrapper = dataWrapper
+        }
+        
+        wrapper.fetchingCondition.lock()
+        
+        if let object = dataCache.get(wrapper) {
+            wrapper.fetchingCondition.unlock()
+            completion(object, .CACHED)
+        }
+        else if wrapper.isFetching {
+            wrapper.fetchingCondition.unlock()
+            Thread {
+                wrapper.fetchingCondition.lock()
+                
+                while wrapper.isFetching { wrapper.fetchingCondition.wait() }
+                
+                wrapper.fetchingCondition.unlock()
+                completion(self.dataCache.get(wrapper), .CACHED)
+                }.start()
+        }
+        else {
+            wrapper.isFetching = true
+            let path = wrapper.dataParent.urlPath
+            wrapper.fetchingCondition.unlock()
+            
+            URLSession.shared.dataTask(with: URL(string: path)!) { (data, response, error) in
+                wrapper.fetchingCondition.lock()
+                
+                var completionObject: ProcessedData?
+                var result: CacheFetchResult = .BADREQUEST
+                
+                defer {
+                    wrapper.fetchingCondition.broadcast()
+                    wrapper.fetchingCondition.unlock()
+                    completion(completionObject, result)
+                }
+                
+                //test for successful network request
+                if let data = data,
+                    error == nil,
+                    [200,204].contains((response as? HTTPURLResponse)?.statusCode)
+                {
+                    completionObject = T.processData(data: data)
+                    if completionObject != nil {
+                        result = .FETCHED
+                        self.dataCache.set(wrapper, completionObject!)
+                    }
+                    else {
+                        result = .DATAPROCESSINGISSUE
+                    }
+                    wrapper.isFetching = false
+                }
+                
+                
+                }.resume()
+        }
+    }
+}
+
+///Caps cached items at a certain value. Inserting an element in a full cache
+///will trim one element from the cache in FIFO order
+fileprivate class CappedCache <T: CachedDataOwner> {
+    
+    typealias Wrapper = DataWrapper<T>
+    typealias ProcessedData = T.ProcessedData
+    
+    private var cache: [Wrapper : ProcessedData] = [:]
+    private var maxSize: Int
+    private var queue = Queue<Wrapper>()
+    
+    init(maxSize: Int = 25) {
+        self.maxSize = maxSize
+    }
+    
+    ///Sets the element for the value and trims the cache if necessary
+    func set(_ key: Wrapper,_ value: ProcessedData) {
+        if queue.getSize() == maxSize {
+            let expiredKey = queue.getFront()!
+            expiredKey.invalidate()
+            cache.removeValue(forKey: expiredKey)
+        }
+        queue.insert(key)
+        cache[key] = value
+    }
+    
+    ///Gets the element for the value in the cache if
+    ///the element exists
+    func get(_ key: Wrapper) -> ProcessedData? {
+        return cache[key]
+    }
+    
+    ///Updates the max size and trims the cache if necessary
+    func updateMaxSize(maxSize: Int) {
+        self.maxSize = maxSize
+        while queue.getSize() > maxSize {
+            let expiredKey = queue.getFront()!
+            expiredKey.invalidate()
+            cache.removeValue(forKey: expiredKey)
+        }
+    }
+}
+
+fileprivate class DataWrapper<T: CachedDataOwner>: Hashable {
+    var hashValue: Int { return dataParent.hashValue }
+    
+    fileprivate var fetchingCondition =  NSCondition()
+    fileprivate var isFetching = false
+    weak var dataCache: DataCache<T>?
+    let dataParent: T
+    
+    init(_ dataParent: T,_ dataCache: DataCache<T>) {
+        self.dataParent = dataParent
+        self.dataCache = dataCache
+    }
+    
+    static func == (lhs: DataWrapper, rhs: DataWrapper) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
+    
+    func invalidate() {
+        dataCache?.parentWrapperDict.removeValue(forKey: dataParent.hashValue)
+    }
+}
+
 ///Your generic FIFO linked list based queue
 fileprivate class Queue<T>
 {
@@ -53,159 +217,5 @@ fileprivate class Queue<T>
     /// - returns: Size of the queue
     public func getSize() -> Int {
         return size
-    }
-}
-
-open class CachedValueParent<V>: Hashable {
-    
-    open var hashValue: Int { get { return id } }
-    
-    open let id: Int
-    
-    ///For internal use only
-    fileprivate var fetchingCondition =  NSCondition()
-    fileprivate var isFetching = false
-    
-    ///Url path for fetching data with a URLSession.dataTask
-    fileprivate var urlPath: String?
-    ///Called when the cache is about to invalidate this object
-    open func willInvalidate() {}
-    
-    ///Required init for proper usage of cache
-    ///- parameter id: unique identifier to use in hashing function
-    required public init (id: Int, urlPath: String?) {
-        self.id = id
-        self.urlPath = urlPath
-    }
-    
-    public static func == (lhs: CachedValueParent, rhs: CachedValueParent) -> Bool {
-        return lhs.id == rhs.id
-    }
-    
-    ///Used to update the urlPath
-    open var getSetUrlPath: String? {
-        get { return urlPath }
-        set(newPath) { self.urlPath = newPath }
-    }
-    
-    ///Process the data received from the network request
-    open func processData(data: Data) -> V? {
-        return nil
-    }
-}
-
-///Caps cached items at a certain value. Inserting an element in a full cache
-///will trim one element from the cache in FIFO order
-fileprivate class CappedCache<K: CachedValueParent<V>, V> {
-    
-    private var cache: [K:V] = [:]
-    private var maxSize: Int
-    private var queue = Queue<K>()
-    
-    init(maxSize: Int = 25) {
-        self.maxSize = maxSize
-    }
-    
-    ///Sets the element for the value and trims the cache if necessary
-    func set(_ key: K,_ value: V) {
-        if queue.getSize() == maxSize {
-            let expiredKey = queue.getFront()!
-            expiredKey.willInvalidate()
-            cache.removeValue(forKey: expiredKey)
-        }
-        queue.insert(key)
-        cache[key] = value
-    }
-    
-    ///Gets the element for the value in the cache if
-    ///the element exists
-    func get(_ key: K) -> V? {
-        return cache[key]
-    }
-    
-    ///Updates the max size and trims the cache if necessary
-    func updateMaxSize(maxSize: Int) {
-        self.maxSize = maxSize
-        while queue.getSize() > maxSize {
-            let expiredKey = queue.getFront()!
-            expiredKey.willInvalidate()
-            cache.removeValue(forKey: expiredKey)
-        }
-    }
-}
-
-enum CacheFetchResult {
-    case CACHED, FETCHED, NILPATH, BADREQUEST, DATAPROCESSINGISSUE
-}
-
-///Used to limit the size of the data cache
-///while minimizing the number of network requests
-open class DataCache<V> {
-    private var dataCache: CappedCache<CachedValueParent<V>,V>
-    
-    init(maxSize: Int) {
-        self.dataCache = CappedCache<CachedValueParent<V>, V>(maxSize: maxSize)
-    }
-    
-    ///Returns the profile photo if available.
-    /// - parameter parent: parent object to fetch data for
-    /// - parameter completion: NOT GUARANTEED TO RUN ON MAIN THREAD
-    func fetchData(forParent parent: CachedValueParent<V>, _ completion: @escaping (V?, CacheFetchResult) -> Void) {
-        parent.fetchingCondition.lock()
-        
-        if let object = dataCache.get(parent) {
-            parent.fetchingCondition.unlock()
-            completion(object, .CACHED)
-        }
-        else if parent.isFetching {
-            parent.fetchingCondition.unlock()
-            Thread {
-                parent.fetchingCondition.lock()
-                
-                while parent.isFetching { parent.fetchingCondition.wait() }
-                
-                parent.fetchingCondition.unlock()
-                completion(self.dataCache.get(parent), .CACHED)
-                }.start()
-        }
-        else if let path = parent.urlPath {
-            parent.isFetching = true
-            parent.fetchingCondition.unlock()
-            
-            URLSession.shared.dataTask(with: URL(string: path)!) { (data, response, error) in
-                parent.fetchingCondition.lock()
-                
-                var completionObject: V?
-                var result: CacheFetchResult = .BADREQUEST
-                
-                defer {
-                    parent.fetchingCondition.broadcast()
-                    parent.fetchingCondition.unlock()
-                    completion(completionObject, result)
-                }
-                
-                //test for successful network request
-                if let data = data,
-                    error == nil,
-                    [200,204].contains((response as? HTTPURLResponse)?.statusCode)
-                {
-                    completionObject = parent.processData(data: data)
-                    if completionObject != nil {
-                        result = .FETCHED
-                        self.dataCache.set(parent, completionObject!)
-                    }
-                    else {
-                        result = .DATAPROCESSINGISSUE
-                    }
-                    parent.isFetching = false
-                }
-                
-                
-                }.resume()
-        }
-        else {
-            parent.fetchingCondition.unlock()
-            completion(nil, .NILPATH)
-        }
     }
 }
